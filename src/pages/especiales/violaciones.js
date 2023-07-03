65d0547e00404a9fe271b42037a5052c6d315364
YaNiPaper <hectorpina@protonmail.com>
2023-07-03 14:23:47 -0600

    usando potro metodo

--- src/pages/especiales/violaciones.js
+++ src/pages/especiales/violaciones.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+import './resources/ol.css';
+import './resources/fontawesome-all.min.css';
+import './resources/horsey.min.css';
+import './resources/ol3-search-layer.min.css';
+import './resources/ol-layerswitcher.css';
+import './resources/qgis2web.css';
+import qgis2webExpressions from './resources/qgis2web_expressions.js';
+import polyfills from './resources/polyfills.js';
+import functions from './resources/functions.js';
+import ol from './resources/ol.js';
+import horsey from './resources/horsey.min.js';
+import ol3SearchLayer from './resources/ol3-search-layer.js';
+import olLayerSwitcher from './resources/ol-layerswitcher.js';
+import zmgMpos1 from './layers/ZMG_mpos_1.js';
+import zmgColonias2 from './layers/ZMG_Colonias_2.js';
+import delitosvsmujeresZmg3 from './layers/delitosvsmujeres_zmg_3.js';
+import zmgMpos1Style from './styles/ZMG_mpos_1_style.js';
+import zmgColonias2Style from './styles/ZMG_Colonias_2_style.js';
+import delitosvsmujeresZmg3Style from './styles/delitosvsmujeres_zmg_3_style.js';
+import layers from './layers/layers.js';
+import autolinker from './resources/Autolinker.min.js';
+import qgis2web from './resources/qgis2web.js';
+
 const SpecialPage = () => {
   return (
     <>
@@ -10,24 +33,6 @@ const SpecialPage = () => {
           <div id="popup-content"></div>
         </div>
       </div>
-
-      <script src="./resources/qgis2web_expressions.js"></script>
-      <script src="./resources/polyfills.js"></script>
-      <script src="./resources/functions.js"></script>
-      <script src="./resources/ol.js"></script>
-      <script src="http://cdn.polyfill.io/v2/polyfill.min.js?features=Element.prototype.classList,URL"></script>
-      <script src="./resources/horsey.min.js"></script>
-      <script src="./resources/ol3-search-layer.js"></script>
-      <script src="./resources/ol-layerswitcher.js"></script>
-      <script src="./layers/ZMG_mpos_1.js"></script>
-      <script src="./layers/ZMG_Colonias_2.js"></script>
-      <script src="./layers/delitosvsmujeres_zmg_3.js"></script>
-      <script src="./styles/ZMG_mpos_1_style.js"></script>
-      <script src="./styles/ZMG_Colonias_2_style.js"></script>
-      <script src="./styles/delitosvsmujeres_zmg_3_style.js"></script>
-      <script src="./layers/layers.js" type="text/javascript"></script>
-      <script src="./resources/Autolinker.min.js"></script>
-      <script src="./resources/qgis2web.js"></script>
     </>
   );
 };
