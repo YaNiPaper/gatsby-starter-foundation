072dc480c2b46c5a8282a96bc7cf804e8dcd1acd
YaNiPaper <hectorpina@protonmail.com>
2023-07-03 14:05:13 -0600

    movi publico a raiz

--- src/pages/especiales/violaciones.js
+++ src/pages/especiales/violaciones.js
@@ -11,23 +11,23 @@ const SpecialPage = () => {
         </div>
       </div>
 
-      <script src="./src/publico/archivos/resources/qgis2web_expressions.js"></script>
-      <script src="./src/publico/archivos/resources/polyfills.js"></script>
-      <script src="./src/publico/archivos/resources/functions.js"></script>
-      <script src="./src/publico/archivos/resources/ol.js"></script>
+      <script src="./publico/archivos/resources/qgis2web_expressions.js"></script>
+      <script src="./publico/archivos/resources/polyfills.js"></script>
+      <script src="./publico/archivos/resources/functions.js"></script>
+      <script src="./publico/archivos/resources/ol.js"></script>
       <script src="http://cdn.polyfill.io/v2/polyfill.min.js?features=Element.prototype.classList,URL"></script>
-      <script src="./src/publico/archivos/resources/horsey.min.js"></script>
-      <script src="./src/publico/archivos/resources/ol3-search-layer.js"></script>
-      <script src="./src/publico/archivos/resources/ol-layerswitcher.js"></script>
-      <script src="./src/publico/archivos/layers/ZMG_mpos_1.js"></script>
-      <script src="./src/publico/archivos/layers/ZMG_Colonias_2.js"></script>
-      <script src="./src/publico/archivos/layers/delitosvsmujeres_zmg_3.js"></script>
-      <script src="./src/publico/archivos/styles/ZMG_mpos_1_style.js"></script>
-      <script src="./src/publico/archivos/styles/ZMG_Colonias_2_style.js"></script>
-      <script src="./src/publico/archivos/styles/delitosvsmujeres_zmg_3_style.js"></script>
-      <script src="./src/publico/archivos/layers/layers.js" type="text/javascript"></script>
-      <script src="./src/publico/archivos/resources/Autolinker.min.js"></script>
-      <script src="./src/publico/archivos/resources/qgis2web.js"></script>
+      <script src="./publico/archivos/resources/horsey.min.js"></script>
+      <script src="./publico/archivos/resources/ol3-search-layer.js"></script>
+      <script src="./publico/archivos/resources/ol-layerswitcher.js"></script>
+      <script src="./publico/archivos/layers/ZMG_mpos_1.js"></script>
+      <script src="./publico/archivos/layers/ZMG_Colonias_2.js"></script>
+      <script src="./publico/archivos/layers/delitosvsmujeres_zmg_3.js"></script>
+      <script src="./publico/archivos/styles/ZMG_mpos_1_style.js"></script>
+      <script src="./publico/archivos/styles/ZMG_Colonias_2_style.js"></script>
+      <script src="./publico/archivos/styles/delitosvsmujeres_zmg_3_style.js"></script>
+      <script src="./publico/archivos/layers/layers.js" type="text/javascript"></script>
+      <script src="./publico/archivos/resources/Autolinker.min.js"></script>
+      <script src="./publico/archivos/resources/qgis2web.js"></script>
     </>
   );
 };
