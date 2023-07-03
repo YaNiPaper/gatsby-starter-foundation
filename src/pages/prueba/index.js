993b13d9f26cf89857caabe4c7d7e5b3b2218ee1
YaNiPaper <134749730+YaNiPaper@users.noreply.github.com>
2023-07-03 03:25:40 -0600

    Prueba index js

---
+++ src/pages/prueba/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SpecialPage = () => {
+  // Código de tu componente SpecialPage
+  return (
+    <div>
+      <h1>Página Especial</h1>
+      <p>Este es mi contenido especial.</p>
+    </div>
+  );
+};
+
+export default SpecialPage;
