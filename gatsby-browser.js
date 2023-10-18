e708731dcad2c381749753276ac3bbb71e550123
YaNiPaper <134749730+YaNiPaper@users.noreply.github.com>
2023-10-17 19:41:52 -0600

    Update gatsby-browser.js Cambié el mensaje de actualización

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
+    `Esta página ha sido actualizada. ` +
+      `¿Quieres refrescar para ver la última versión?`
   )
 
   if (answer === true) {
